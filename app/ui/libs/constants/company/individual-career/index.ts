04f8b102e6ec88cf34bdab9b0b8a8a9279ca591a
Darshan Chobarkar <darshan@trubotai.com>
2025-07-22 19:51:40 +0530

    refactor: sort

--- app/ui/libs/constants/company/individual-career/index.ts
+++ app/ui/libs/constants/company/individual-career/index.ts
@@ -1,12 +1,22 @@
 import { CareerPageDataType } from "../../../types/careerPage";
 
+import { aiSolutionArchitectPageData } from "./architect-ai-solutions";
 import { accountsPayrollManagerPageData } from "./manager-accounts-payroll";
+import { executiveAssistantPageData } from "./assistant-operations-founder";
+import { financeAdvisorPageData } from "./board-advisor-finance";
+import { legalAdvisorPageData } from "./board-advisor-legal";
+import { salesGrowthAdvisorPageData } from "./board-advisor-sales-growth";
+import { technologyAdvisorPageData } from "./board-advisor-technology";
+import { cfoPageData } from "./chief-financial-officer";
 import { COOPageData } from "./chief-operations-officer";
 import { CSOPageData } from "./chief-sales-officer";
 import { CTOPageData } from "./chief-technology-officer";
+import { cacsManagerPageData } from "./manager-ca-cs";
 import { customerSuccessManagerPageData } from "./manager-operations-customer-success";
-import { executiveAssistantPageData } from "./assistant-operations-founder";
-import { financeAdvisorPageData } from "./board-advisor-finance";
+import { graphicDesignerPageData } from "./designer-graphic";
+import { uiuxDesignerPageData } from "./designer-uiux";
+import { MLEngineerPageData } from "./engineer-machine-learning";
+import { softwareEngineerPageData } from "./engineer-software";
 import { fullStackDeveloperInternPageData } from "./intern-technology-fullstack-developer";
 import { fullStackEngineeringManagerPageData } from "./manager-technology-fullstack-engineering";
 import { fundraisingManagerPageData } from "./manager-fundraising";
@@ -18,30 +28,20 @@ import { headSalesCountryPageData } from "./head-sales-country";
 import { HRInternPageData } from "./intern-human-resources";
 import { humanResourcesManagerPageData } from "./manager-human-resources";
 import { leadProductDesignerPageData } from "./lead-product-designer";
-import { legalAdvisorPageData } from "./board-advisor-legal";
+import { socialMediaStrategyLeadPageData } from "./lead-social-media-strategy";
 import { MASpecialistManagerPageData } from "./manager-mergers-acquisitions-specialis";
-import { MLEngineerPageData } from "./engineer-machine-learning";
 import { productManagerPageData } from "./manager-product";
 import { productMarketingInternPageData } from "./intern-product-marketing";
 import { projectManagerPageData } from "./manager-project";
-import { salesGrowthAdvisorPageData } from "./board-advisor-sales-growth";
 import { salesMarketingInternPageData } from "./intern-sales-marketing";
-import { softwareEngineerPageData } from "./engineer-software";
-import { technologyAdvisorPageData } from "./board-advisor-technology";
-import { performanceMarketerPageData } from "./marketer-performance";
-import { socialMediaStrategyLeadPageData } from "./lead-social-media-strategy";
-import { vpTechnologyPageData } from "./vp-technology";
-import { svpTechnologyAIPageData } from "./svp-technology-ai";
-import { graphicDesignerPageData } from "./designer-graphic";
-import { uiuxDesignerPageData } from "./designer-uiux";
-import { cfoPageData } from "./chief-financial-officer";
-import { aiSolutionArchitectPageData } from "./architect-ai-solutions";
-import { cacsManagerPageData } from "./manager-ca-cs";
 import { seniorFullStackDeveloperPageData } from "./senior-full-stack-developer";
+import { svpTechnologyAIPageData } from "./svp-technology-ai";
+import { vpTechnologyPageData } from "./vp-technology";
+import { performanceMarketerPageData } from "./marketer-performance";
 
 export const careerPagesMap: Record<string, CareerPageDataType> = {
-  "assistant-operations-founder": executiveAssistantPageData,
   "architect-ai-solutions": aiSolutionArchitectPageData,
+  "assistant-operations-founder": executiveAssistantPageData,
   "board-advisor-finance": financeAdvisorPageData,
   "board-advisor-legal": legalAdvisorPageData,
   "board-advisor-sales-growth": salesGrowthAdvisorPageData,
@@ -52,6 +52,8 @@ export const careerPagesMap: Record<string, CareerPageDataType> = {
   "chief-technology-officer": CTOPageData,
   "designer-graphic": graphicDesignerPageData,
   "designer-uiux": uiuxDesignerPageData,
+  "engineer-machine-learning": MLEngineerPageData,
+  "engineer-software": softwareEngineerPageData,
   "head-fundraising": headFundraisingPageData,
   "head-human-resources": headHumanResourcesPageData,
   "head-mergers-acquisitions": headMAPageData,
@@ -73,9 +75,7 @@ export const careerPagesMap: Record<string, CareerPageDataType> = {
   "manager-project": projectManagerPageData,
   "manager-technology-fullstack-engineer": fullStackEngineeringManagerPageData,
   "marketer-performance": performanceMarketerPageData,
-  "engineer-machine-learning": MLEngineerPageData,
-  "engineer-software": softwareEngineerPageData,
+  "senior-full-stack-developer": seniorFullStackDeveloperPageData,
   "svp-technology-ai": svpTechnologyAIPageData,
   "vp-technology": vpTechnologyPageData,
-  "senior-full-stack-developer": seniorFullStackDeveloperPageData,
 };
