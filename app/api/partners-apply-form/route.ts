4b288c73bc79f35b0d4dd6a687434184e0398496
Darshan Chobarkar <darshan@trubotai.com>
2025-07-23 18:10:54 +0530

    fix: create partner apply form endpoint

--- app/api/partners-apply-form/route.ts
+++ app/api/partners-apply-form/route.ts
@@ -3,15 +3,30 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
   try {
     const data = await req.json();
-    console.log(data);
-    const res = await fetch("", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        ...data,
-        source: "Partners Apply Form",
-      }),
-    });
+    const res = await fetch(
+      "https://crm.trubotai.com/api/v1/LeadCapture/359fe9482c1cbe561b271f0bf918aa4a",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          cCompanyName: data.companyName,
+          website: data.website,
+          industry: data.industry,
+          cCompanySize: data.companySize,
+          cPartnershipType: data.partnershipType,
+          cPartnershipGoals: data.partnershipGoals,
+          firstName: data.firstName,
+          lastName: data.lastName,
+          emailAddress: data.email,
+          phoneNumber: data.phone,
+          cJobTitle: data.jobTitle,
+          cCurrentCustomerBase: data.currentCustomers,
+          cTechnicalCapabilities: data.technicalCapabilities,
+          description: data.questions,
+          cSource: "Partners Apply Form",
+        }),
+      }
+    );
 
     if (!res.ok)
       return NextResponse.json({ error: "Failed to submit" }, { status: 500 });
